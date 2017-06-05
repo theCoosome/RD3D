98a4984fe46ee90d581600d7f6f11677d46d3c63
Coosome Joe <thecoltonjelsema@gmail.com>
2017-06-05 15:21:45 -0700

    A bunch of unity stuff
    also its the first push/commit

---
+++ RD3D/Assets/Room.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour {
+
+    [SerializeField]
+    public bool plant;
+    public bool manmade;
+    public bool water;
+    public bool dark;
+    public bool animal;
+    new public bool light;
+    public bool items;
+
+    public bool north;
+    public bool east;
+    public bool south;
+    public bool west;
+
+    public int sanity;
+    public bool isref;
+    public bool normal;
+
+    public Room(bool pPlant, bool pManmade, bool pWater, bool pDark, bool pAnimal, bool pLight, bool pItems, bool North, bool East, bool South, bool West, int sane, bool reference = false, bool norm = true)
+    {
+        plant = pPlant;
+        manmade = pManmade;
+        water = pWater;
+        dark = pDark;
+        animal = pAnimal;
+        light = pLight;
+        items = pItems;
+        north = North;
+        east = East;
+        south = South;
+        west = West;
+
+        sanity = sane;
+        isref = reference;
+        normal = norm;
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RD3D/Assets/RoomGen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomGen : MonoBehaviour {
+
+    public List<Room> rooms = new List<Room>();
+    public Room roomStudy;
+    public Room roomHallway;
+    public Room roomBathroom;
+    public Room roomtrue = new Room(false, true, false, false, false, false, false, false, true, true, true, -4); //lit black
+    public Room room2 = new Room(false, true, false, false, false, true, true, false, true, false, false, 0); // linoleum cube
+    public Room room3 = new Room(false, false, false, false, true, true, false, true, false, false, false, 1); // walls cave inward
+
+    // Use this for initialization
+    void Start ()
+    {
+        //refresh player
+        Room newRoom = GameObject.Instantiate<Room>(roomStudy); // random room in study place
+
+        rooms.Add(newRoom);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
